ae899cb3097094721dce97c83e12e258de2860fb
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-02-07 17:34:58 +0200

    done task-5

--- js/task-04.js
+++ js/task-04.js
@@ -7,14 +7,8 @@
 // Обновляй интерфейс новым значением переменной counterValue.
 let counterValue = 0;
 const action = document.querySelectorAll('button');
-// console.log(action);
-// console.log(action[0].dataset.action);
-// console.log(action[1].dataset.action);
-
 const buttonMinus = document.querySelector('button[data-action="decrement"]');
-// console.log(buttonMinus);
 const buttonPlus = document.querySelector('button[data-action="increment"]');
-// console.log(buttonPlus);
 const value = document.querySelector('#value');
 
 const eventDecrement = () => {
--- js/task-05.js
+++ js/task-05.js
@@ -11,25 +11,14 @@
 const nameInput = document.querySelector('#name-input');
 
 const nameOutput = document.querySelector('#name-output');
+
 console.log(nameOutput.textContent);
-
-nameInput.addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
+nameInput.addEventListener('input', event => {
     event.preventDefault();
-
-    const inputElem = event.currentTarget.elements;
-    console.log(inputElem);
-
-    if (login.value === '' || password.value === '') {
-        return console.log('Please fill in all the fields!');
-    }
-
-    console.log(`Login: ${login.value}, Password: ${password.value}`);
-    event.currentTarget.reset();
-}
-
-// console.log(nameOutput.textContent);
-// if (!userInput) {
-//     nameOutput.textContent
-// };
+    
+    console.log(event.currentTarget.value);
+    const userInput = event.currentTarget.value;
+    nameOutput.textContent = userInput;
+    if (!userInput) return;
+    
+});

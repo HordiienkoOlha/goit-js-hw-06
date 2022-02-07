9892c92849c1e5d34082d6f9c04691814da5e510
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-02-07 17:58:57 +0200

    edit task-5

--- js/task-05.js
+++ js/task-05.js
@@ -8,17 +8,33 @@
 // Если инпут пустой, в спане должна
 // отображаться строка "Anonymous".
 
-const nameInput = document.querySelector('#name-input');
+// const nameInput = document.querySelector('#name-input');
 
-const nameOutput = document.querySelector('#name-output');
+// const nameOutput = document.querySelector('#name-output');
 
-console.log(nameOutput.textContent);
-nameInput.addEventListener('input', event => {
-    event.preventDefault();
+// console.log(nameOutput.textContent);
+// nameInput.addEventListener('input', event => {
+//     event.preventDefault();
     
-    console.log(event.currentTarget.value);
-    const userInput = event.currentTarget.value;
-    nameOutput.textContent = userInput;
-    if (!userInput) return;
+//     console.log(event.currentTarget.value);
+//     const userInput = event.currentTarget.value;
+//     nameOutput.textContent = userInput;
+
+//     console.log(userInput.trim());
+//     if (!userInput) return;
     
-});
+// });
+
+// const nameInput = document.querySelector('#name-input');
+// const nameOutput = document.querySelector('#name-output');
+
+// nameInput.addEventListener('input', inputEvent)
+
+// function inputEvent(event) {
+//     event.preventDefault();
+
+//     const userInput = event.currentTarget.value;
+//     nameOutput.textContent = userInput;
+// console.log(userInput);
+//     if (!userInput) return;
+// }

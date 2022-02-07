86c37b0634a8560860961f6e671b3b443b324b86
HordiienkoOlha <91278187+HordiienkoOlha@users.noreply.github.com>
2022-02-07 22:44:29 +0200

    done task - 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,17 @@
-// Напиши скрипт, который реагирует на изменение 
-// значения input#font - size - control(событие input) 
-// и изменяет инлайн - стиль span#text обновляя 
-// свойство font - size.В результате при перетаскивании 
+// Напиши скрипт, который реагирует на изменение
+// значения input#font - size - control(событие input)
+// и изменяет инлайн - стиль span#text обновляя
+// свойство font - size.В результате при перетаскивании
 // ползунка будет меняться размер текста.
 
 const inputRange = document.querySelector('#font-size-control');
-console.log(inputRange);
+const text = document.querySelector('#text');
+// console.log(text);
 
+inputRange.addEventListener('input', editFontSize);
 
-inputText.addEventListener('blur', editStyle);
+function editFontSize(event) {
+    const userInput = event.currentTarget.value;
+    // console.log(userInput);
+    text.style.fontSize = `${userInput}px`;
+}

26d0374bec4bcf18bfe2107cab8edbe6e90d8ee1
HordiienkoOlha <olhahordiienkomail@gmail.com>
2022-02-07 16:23:04 +0200

    update task-05

--- js/task-05.js
+++ js/task-05.js
@@ -9,4 +9,27 @@
 // отображаться строка "Anonymous".
 
 const nameInput = document.querySelector('#name-input');
-console.log(nameInput)
+
+const nameOutput = document.querySelector('#name-output');
+console.log(nameOutput.textContent);
+
+nameInput.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const inputElem = event.currentTarget.elements;
+    console.log(inputElem);
+
+    if (login.value === '' || password.value === '') {
+        return console.log('Please fill in all the fields!');
+    }
+
+    console.log(`Login: ${login.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
+
+// console.log(nameOutput.textContent);
+// if (!userInput) {
+//     nameOutput.textContent
+// };
